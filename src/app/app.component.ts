dd7ce8d6526fba7e414d35ac23282efa2dc80370
nouha ismaili <nouhasmaili3@gmail.com>
2024-11-11 10:55:30 +0100

    modif2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { GameAddComponent } from './application/components/game-add/game-add.component';
-import { EmployeAddComponent } from './exercice/components/employe-add/employe-add.component';
-import {  HttpClientModule } from '@angular/common/http';
+import { EmployeAddComponent } from './exercice1/components/employe-add/employe-add.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HttpClientModule, GameAddComponent, EmployeAddComponent],
+  imports: [RouterOutlet, GameAddComponent, EmployeAddComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
